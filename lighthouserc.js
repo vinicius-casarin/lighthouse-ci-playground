cd0556d277321f1b260f534bf384636294d3a32e
vinicius-casarin <vinicius.casarin@kabum.com.br>
2024-11-19 18:37:06 -0300

    chore: lhci collect params

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,11 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'npm run start',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
   },
-};
+}
