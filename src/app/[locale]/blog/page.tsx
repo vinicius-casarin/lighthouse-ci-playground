2675f994f6d15429c7108f848bddbfd413b79c77
vinicius-casarin <vinicius.casarin@kabum.com.br>
2024-11-22 10:25:31 -0300

    test comment on pr

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -77,6 +77,7 @@ export default function Blog(
                 marginBottom="l"
                 variant="display-strong-s">
                 {blog.title}
+                {blog.title}
             </Heading>
 			<Flex
 				fillWidth flex={1} direction="column">
